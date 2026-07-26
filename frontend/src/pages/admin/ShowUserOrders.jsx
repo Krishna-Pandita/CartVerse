4f28db889e61bc4c8acf4a70b614cdb4ca5d29dd
Krishna Pandita <kpandita68@gmail.com>
2026-07-26 15:17:34 +0530

    feat: implement ShowUserOrders component

--- frontend/src/pages/admin/ShowUserOrders.jsx
+++ frontend/src/pages/admin/ShowUserOrders.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import OrderCard from "@/components/OrderCard";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const ShowUserOrders = () => {
-  return (
-    <div>ShowUserOrders</div>
-  )
-}
+  const [userOrder, setUserOrder] = useState([]);
+  const { userId } = useParams();
 
-export default ShowUserOrders
+  const getUserOrders = async () => {
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+
+      const res = await axios.get(
+        `${import.meta.env.VITE_URL}/api/v1/orders/user-order/${userId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      setUserOrder(res.data.orders);
+      // or setUserOrder(res.data.orders); depending on your API
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserOrders();
+  }, [userId]);
+
+  return (
+    <div className="pl-[350px] py-26">
+      <OrderCard userOrder={userOrder} />
+    </div>
+  );
+};
+
+export default ShowUserOrders;

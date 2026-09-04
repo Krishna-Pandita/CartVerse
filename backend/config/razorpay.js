f5f68843b30231572aa16ad9a648d04878e60b91
Krishna Pandita <kpandita68@gmail.com>
2026-09-04 21:49:25 +0530

    Add razorpay config file

--- backend/config/razorpay.js
+++ backend/config/razorpay.js
@@ -1,5 +1,8 @@
 import Razorpay from "razorpay";
 
+console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
+console.log("SECRET:", process.env.RAZORPAY_SECRET);
+
 const razorpayInstance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_SECRET,

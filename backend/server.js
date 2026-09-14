41d6751e16ea8727bd4a404835a9c2e4902ae96c
Krishna Pandita <kpandita68@gmail.com>
2026-09-14 22:32:22 +0530

    Change CORS origin to 'https://cartverse-1.onrender.com'
    Updated CORS origin to a specific frontend URL.

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: "https://cartverse-1.onrender.com",
   credentials:true                           // Update with your frontend URL
 }))
 
